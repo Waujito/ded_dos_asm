a19812cf4ec9b2ff6f16022ffde083b4a39917d1
Vadim Vetrov <vetrovvd@gmail.com>
2026-02-11 03:09:03 +0300

    Border for args

--- vram.asm
+++ vram.asm
@@ -4,6 +4,8 @@ W_HEIGHT	equ 25
 W_WIDTH		equ 80
 FILLER_SYM	equ 0503h ; Pink heart, used for debugging. Replace with 0000 to clear the area
 
+BORDER_COLOR equ 0bh
+
 .code
 org 100h
 
@@ -12,23 +14,33 @@ Start:
 		mov ax, 0b800h
 		mov es, ax
 
-		mov di, 5
+		mov di, 8
 		call ShiftText
 
 		mov di, ax
+		add di, 10
+		mov si, 50
+		mov dx, 8
+		call FillBorder
 
-		mov bx, ax
-		mov ax, 0ce31h
-		mov es:[bx], ax
+		
+		; Starting of row-wised width
+		add di, 2 * 2 * W_WIDTH + 2 * 2
+		mov bx, si
+		sub bx, 4
+		shl bx, 1
+		add bx, di
+		mov si, bx
 
 		mov dl, ds:[80h]
-
 
 		test dl, dl
 		jz .pw_loop_end
 
 		dec dl
 		
+		push di
+		push di
 		xor cx, cx
 .pw_loop_start:
 		cmp cx, dx
@@ -38,18 +50,161 @@ Start:
 		mov al, ds:[bx + 82h]
 		mov ah, 0ceh
 
-		mov bx, cx
-		shl bx, 1
-		add bx, di
-		mov es:[bx], ax
+		mov es:[di], ax
+		add di, 2
+
+		cmp di, si
+		jle .pw_loop_no_act
+
+		pop di
+		add di, 2 * W_WIDTH
+		push di
+		add si, 2 * W_WIDTH
+	
+.pw_loop_no_act:
+
 
 		inc cx
 		jmp .pw_loop_start
 .pw_loop_end:
 
+		pop di
+		pop di
+
+
 
 		mov ax, 4c00h
 		int 21h
+
+FillBorder:
+; DI is starting point
+; SI is nrows
+; DX is ncols
+
+	mov ah, BORDER_COLOR
+	mov al, 0cdh
+
+; fill top row
+	xor cx, cx
+.fill_top_row:
+	mov bx, cx
+	shl bx, 1
+	add bx, di
+
+	mov es:[bx], ax
+
+	inc cx
+	cmp cx, si
+	jl .fill_top_row
+; end fill top row
+
+	push di
+	push ax
+	push dx
+
+	mov ax, W_WIDTH * 2
+	dec dx
+	mul dx
+	add di, ax
+
+	pop dx
+	pop ax
+
+; fill bottom row
+	xor cx, cx
+.fill_bottom_row:
+	mov bx, cx
+	shl bx, 1
+	add bx, di
+
+	mov es:[bx], ax
+
+	inc cx
+	cmp cx, si
+	jl .fill_bottom_row
+; end fill bottom row
+
+	pop di
+	mov al, 0bah
+	push si
+
+	mov si, W_WIDTH * 2
+; fill left border
+	xor cx, cx
+	mov bx, di
+.fill_left_brd:
+	mov es:[bx], ax
+
+	add bx, si
+	inc cx
+	cmp cx, dx
+	jl .fill_left_brd
+
+	pop si
+	push di
+	push si
+
+	mov bx, si
+	shl bx, 1
+
+	add di, bx
+
+	mov si, W_WIDTH * 2
+; fill right border
+	xor cx, cx
+	mov bx, di
+.fill_right_brd:
+	mov es:[bx], ax
+
+	add bx, si
+	inc cx
+	cmp cx, dx
+	jl .fill_right_brd
+
+	pop si
+	pop di
+
+; fill angles
+	push di
+
+	xor cx, cx
+	mov al, 0c9h
+	mov es:[di], ax
+
+	mov bx, si
+	shl bx, 1
+	add di, bx
+	mov al, 0bbh		; top-right
+	mov es:[di], ax
+
+	pop di
+	push di
+	push si
+	push dx
+
+	mov si, ax
+	mov ax, 2 * W_WIDTH
+	dec dx
+	mul dx
+	add di, ax
+	mov ax, si
+	
+	mov al, 0c8h		; top-right
+	mov es:[di], ax
+
+	pop dx
+	pop si
+
+	mov bx, si
+	shl bx, 1
+	add di, bx
+	mov al, 0bch		; bottom-right
+	mov es:[di], ax
+
+	pop di
+
+	ret
+	
 
 ShiftText:
 ; DI is nrows needed to shift down
